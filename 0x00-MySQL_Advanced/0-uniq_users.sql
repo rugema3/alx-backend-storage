aac65964dd2f1f80967ebddf1576fb3de7ccc799
rugema3 <rugema61@gmail.com>
2024-01-18 18:34:22 +0000

    a sql script that creates a table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+--  a SQL script that creates a table users
+
+-- Create a Table if it doesn't exist
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
+

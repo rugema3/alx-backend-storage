d0acae0135fb996a46c5b0aeb883b8c0a7a62e55
rugema3 <rugema61@gmail.com>
2024-01-24 19:36:51 +0200

    updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,42 +24,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        """
-        Decorator to count the number of times a method is called.
-
-        Args:
-        - method (Callable): The method to be decorated.
-
-        Returns:
-        - Callable: The decorated method.
-        """
-        key = method.__qualname__
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-
-        return wrapper
-
-    @count_calls
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store data in the cache and return a unique key.
-
-        Args:
-        - data (Union[str, bytes, int, float]): The data to be stored in
-                                                the cache.
-
-        Returns:
-        - str: A unique key associated with the stored data.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
-
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """

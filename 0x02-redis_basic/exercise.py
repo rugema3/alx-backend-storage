e79c1f396cbaec308adc66ebdac8465c26ceefb2
rugema3 <rugema61@gmail.com>
2024-01-24 19:39:21 +0200

    updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,6 +24,21 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in the cache and return a unique key.
+
+        Args:
+        - data (Union[str, bytes, int, float]): The data to be stored in
+                                                the cache.
+
+        Returns:
+        - str: A unique key associated with the stored data.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
